9b792e7809ed7e821725e47cb68fb8798e10282d
pkulik0 <pkulik0@icloud.com>
2022-07-16 16:35:05 +0200

    Remove unused helper (Date)

--- Sources/SwiftTwitchAPI/Endpoints/Bits.swift
+++ Sources/SwiftTwitchAPI/Endpoints/Bits.swift
@@ -16,6 +16,16 @@ extension SwiftTwitchAPI {
         let total: Int
 
         let dateRange: DateRange
+        
+        struct DateRange: Codable {
+            let endedAt: String
+            let startedAt: String
+            
+            enum CodingKeys: String, CodingKey {
+                case endedAt = "ended_at"
+                case startedAt = "started_at"
+            }
+        }
     }
     
     struct BitsCheermoteResponse: Codable {
