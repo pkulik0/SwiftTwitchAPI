4321481f13c1cef1e1eebff42842138904bcf2e8
pkulik0 <pkulik0@icloud.com>
2022-07-16 23:04:18 +0200

    place Paginated in an extension

--- Sources/SwiftTwitchAPI/Helpers/Pagination.swift
+++ Sources/SwiftTwitchAPI/Helpers/Pagination.swift
@@ -5,24 +5,26 @@
 //  Created by pkulik0 on 28/06/2022.
 //
 
-public class Paginated<WrappedType: Codable>: Codable {
-    public struct PaginationData: Codable {
-        let cursor: String
-    }
+public extension SwiftTwitchAPI {
+    class Paginated<WrappedType: Codable>: Codable {
+        public struct PaginationData: Codable {
+            let cursor: String
+        }
 
-    public let data: [WrappedType]
-    public let pagination: PaginationData?
-    public let total: Int?
-    
-    public required init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
+        public let data: [WrappedType]
+        public let pagination: PaginationData?
+        public let total: Int?
         
-        pagination = try? values.decode(PaginationData.self, forKey: .pagination)
-        data = (try? values.decode([WrappedType].self, forKey: .data)) ?? []
-        total = try? values.decode(Int.self, forKey: .total)
-    }
-    
-    enum CodingKeys: String, CodingKey {
-        case data, pagination, total
+        public required init(from decoder: Decoder) throws {
+            let values = try decoder.container(keyedBy: CodingKeys.self)
+            
+            pagination = try? values.decode(PaginationData.self, forKey: .pagination)
+            data = (try? values.decode([WrappedType].self, forKey: .data)) ?? []
+            total = try? values.decode(Int.self, forKey: .total)
+        }
+        
+        enum CodingKeys: String, CodingKey {
+            case data, pagination, total
+        }
     }
 }
